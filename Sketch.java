48b16bf47048b31822f864b9c8808a3902d6805b
Jeffrey Lue <78740666+Kyle-Lue@users.noreply.github.com>
2022-02-24 16:02:16 +0000

    k

--- Sketch.java
+++ Sketch.java
@@ -23,13 +23,34 @@ public class Sketch extends PApplet {
    * Called repeatedly, anything drawn to the screen goes here
    */
   public void draw() {
-	  
-	// sample code, delete this stuff
-    stroke(128);
-    line(150, 25, 270, 350);  
+	  //Creating the ground and the colour it will be 
+    fill(0,154,23);
+    rect(width * 0 , height * (float).75 , width * 2,height / 4);
 
-    stroke(255);
-    line(50, 125, 70, 50);  
+    //Creating the sun 
+    fill(247,247,73);
+    ellipse(width / (float)6.15,height / (float)6.15,width / (float)4.44,height / (float)4.44);
+
+    //Creating the house body and colour 
+    fill(188,74,60);
+    rect(width / (float)2.28,height / (float)2.28,width / (float)2.66,height / (float)2.66);
+
+    //Creating the roof and colour of roof
+    fill(109,114,124);
+    triangle(width / (float)2.66,height / (float)2.28,width / (float)1.6,height / 4,width /(float)1.14,height / (float)2.28);
+
+    //Creating door
+    fill(186,140,99);
+    rect(width / (float)1.72,height / (float)1.51,width / 10, height / (float)6.66);
+    
+    //Creating windows
+    fill(199,227,225);
+    rect(width / (float)2.1,height / 2,width / (float)13.33,height / (float)13.33);
+    rect(width / (float)1.42,height / 2,width / (float)13.33,height / (float)13.33);
+    line(width / (float)2.11,height / (float)1.86,width / (float)1.82,height / (float)1.86);
+    line(width / (float)1.92,height / 2,width / (float)1.92,height / (float)1.73);
+    line(width / (float)1.42,height / (float)1.86,width / (float)1.28,height / (float)1.86);
+    line(width / (float)1.35,height / 2,width / (float)1.35,height / (float)1.74);
   }
   
   // define other methods down here.
